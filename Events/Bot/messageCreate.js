8172f408bfe324bf04ba686d982c13105cea568a
GameMaster438 <81307833+GameMaster438@users.noreply.github.com>
2022-06-25 19:16:33 +0200

    messageCreate.js

---
+++ Events/Bot/messageCreate.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const Event = require("../../Structure/Event")
+
+module.exports = new Event("messageCreate", async (bot, message) => {
+
+    const prefix = "!"
+
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0]
+    let args = messageArray.slice(1);
+    let commandFile = bot.commands.get(command.slice(prefix.length))
+
+    if(!message.content.startsWith("!")) return;
+    if(!commandFile) return message.reply(`⚠️ Cette Commande n'existe pas ! ⚠️`)
+
+    commandFile.run(bot, message, args)
+})
